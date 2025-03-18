e8835d5bfe7960eb4890e91b446d0097cfefb60a
Ezequiel <eolavaca@gmail.com>
2025-03-18 20:49:55 -0300

    documentación sobre reduxToolkit thunk y thunk async
    Co-authored-by: nycolop <nycolop@users.noreply.github.com>

--- src/features/productsSlice.ts
+++ src/features/productsSlice.ts
@@ -12,8 +12,26 @@ const initialState: ProductsState = {
   skip: 0,
   productsPerPage: 10,
   products: [],
-  total: 0
+  total: 0,
+  error: false,
+  loading: false
 };
+
+// Documentacion: https://redux-toolkit.js.org/api/createAsyncThunk
+//
+// 1. Crear async thunk
+// export const fetchProducts = createAsyncThunk(
+//   "products/fetchProducts",
+//   async (productId: number, thunkAPI) => {
+//     const response = await fetch("https://miapi.com/products/" + productId);
+//     const json = await response.json();
+//     //devolver lo que yo quería
+//     return json;
+//   }
+// );
+//
+// 3. Llamarlo donde lo necesites (En React) (Dentro de un UseEffect)
+// dispatch(fetchProducts(10))
 
 const productsSlice = createSlice({
   name: "products",
@@ -33,10 +51,50 @@ const productsSlice = createSlice({
     },
     getNewProducts: (state, action: PayloadAction<ProductType[]>) => {
       state.products = action.payload
-    }
+    },
   },
+  // 2. Unirlo al slice
+  // extraReducers: {
+  //   [fetchProducts.pending]: (state, action) => {
+  //     state.error = false;
+  //     state.loading = true;
+  //   },
+  //   [fetchProducts.fulfilled]: (state, action) => {
+  //     state.error = false;
+  //     state.loading = false;
+  //     state.products = action.payload; // return
+  //   },
+  //   [fetchProducts.rejected]: (state, action) => {
+  //     state.error = true;
+  //     state.loading = false;
+  //   },
+  // }
 });
   
-  export const { increment, decrement, getTotal, getNewProducts, updateByPayload } = productsSlice.actions;
-  export default productsSlice.reducer;
+export const { increment, decrement, getTotal, getNewProducts, updateByPayload } = productsSlice.actions;
+export default productsSlice.reducer;
   
+
+// -- Action --
+//   const action = {
+//     type: "INCREASE_COUNTER",
+//     payload: 10
+//   }
+
+//   dispatch({ type: "INCREASE_COUNTER", payload: 10 });
+
+
+// -- Thunk --
+// function increaseCounter(quantity) {
+//   return {
+//     type: "INCREASE_COUNTER",
+//     payload: quantity
+//   }
+// }
+
+// dispatch(increaseCounter(10));
+
+
+// -- Async Thunk -- (Poder peticiones HTTP)
+// Creacion
+
